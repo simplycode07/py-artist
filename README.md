62d46d7e36cbd3dbe636e1a026d6f0ab2d997855
kishlay-notabot <kishlay.work1@gmail.com>
2025-08-03 16:23:31 +0530

    readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pyar-tist
+
+
+this script achieves stippling like effect on any input image, but instead of dots, it places any desired text.  
+this was inspired by a random post online which had somewhat the exact same effect,  
+i knew we can replicate it using code so i made this in a few hours. (yes i have lots of free time to code and work on random projects.)
+
+## input and result:
+
+![comparison_image](comparison.png)
--- img_gen.py
+++ img_gen.py
@@ -1,16 +1,22 @@
-#!/usr/local/bin/python3
+# not the most efficient code but i wrote it in a way so that it's beginner friendly. (im the beginner lol)
 from PIL import Image, ImageDraw
 import numpy as np
 import cv2
 import random
 
 
-
+# read image using opencv
 img = cv2.imread('p.jpeg')
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# block size is used to internally convert this image to a lower resolution, try changing these variables and discover more variations!
 block_size = 32
 height, width = gray_img.shape
+
+# image gets converted to numpy array
 img_array = np.array(gray_img)
+
+# simple iterative function to convert the image to blocks first (make it low resolution basically)
 
 for y in range(0, height, block_size):
     for x in range(0, width, block_size):
@@ -26,8 +32,13 @@ for y in range(0, height, block_size):
         block = img_array[y:y_end, x:x_end]
         avg = int(np.mean(block))
         img_array[y:y_end, x:x_end] = avg
+
+# opencv basic binary thresholding
 ret, thresh_img = cv2.threshold(img_array, 128, 255, cv2.THRESH_BINARY)
 imgtowrite = Image.fromarray(thresh_img).convert("L")
+
+# the cleaning and text adding function, converts the black blocks to white, selects a random coordinate inside that block and dumps it to the array, 
+# then the coordinates are used to place the text in the imaging PIL
 idraw = ImageDraw.Draw(imgtowrite)
 text_positions = []
 for y in range(0, height, block_size):
@@ -52,6 +63,7 @@ for y in range(0, height, block_size):
                 text_positions.append(pos)
 
 for pos in text_positions:
-    idraw.text(pos, "hello world.", fill=0)
+    # customize the text here!
+    idraw.text(pos, "edit this text", fill=0)
 imgtowrite.save('result.png')
             
