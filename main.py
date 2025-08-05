4f487f4c5fe1b5790b2243296d5acf8023abdf15
simplycode07 <simplycode07@gmail.com>
2025-08-05 23:33:30 +0530

    Added color to the text and some performance improvements

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def signof(i):
     return 1 if i > 0 else -1
 
 
-def dis(i):
-    if i == 0:
+def dis(i, j):
+    if i == 0 and j == 0:
         return 0
-    return signof(i) / (i**2)
+    return signof(i) / (j**2 + i**2)
 
 
 def clamp(start: int, value: int, end: int) -> int:
@@ -40,21 +40,25 @@ def clamp(start: int, value: int, end: int) -> int:
 
 
 def convert_image_to_art(original_image):
-    brightness = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    height, width = brightness.shape
+    # brightness = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    brightness = original_image
+    height, width, brr = brightness.shape
     brightness_arr = np.array(brightness)
     img_array = np.zeros(
-        (height//block_size, width//block_size), dtype=np.uint8)
+        (height//block_size, width//block_size, 3), dtype=np.uint8)
 
+    img_array_bw = np.zeros(
+        (height//block_size, width//block_size), dtype=np.int8)
     for y in range(0, height, block_size):
         for x in range(0, width, block_size):
             y_end = min(y + block_size, height)
             x_end = min(x + block_size, width)
 
             block = brightness_arr[y:y_end, x:x_end]
-            avg = int(np.mean(block))
+            avg = block.mean(axis=(0, 1)).astype(np.uint8)
 
-            img_array[y//block_size - 1, x//block_size - 1] = avg
+            img_array[y//block_size - 1, x//block_size - 1] = avg[::-1]
+            img_array_bw[y//block_size - 1, x//block_size - 1] = np.mean(avg) - 128
 
     text_positions = []
 
@@ -80,26 +84,27 @@ def convert_image_to_art(original_image):
                     if clamped_pix_i == pix_i and clamped_pix_j == pix_j:
                         continue
 
-                    brightness_pix = (
-                        int(img_array[clamped_pix_j, clamped_pix_i]) - 127) / 128
+                    # brightness_pix = (
+                    #     int(np.mean(img_array[clamped_pix_j, clamped_pix_i])) - 128) / 128
 
-                    change_pos_x += brightness_pix * \
-                        dis(i) * strength_multiplier
-                    change_pos_y += brightness_pix * \
-                        dis(j) * strength_multiplier
+                    change_pos_x += img_array_bw[clamped_pix_j, clamped_pix_i] * \
+                        dis(i, j) * strength_multiplier
+                    change_pos_y += img_array_bw[clamped_pix_j, clamped_pix_i] * \
+                        dis(j, i) * strength_multiplier
 
             text_pos_x += round(change_pos_x)
             text_pos_y += round(change_pos_y)
 
-            text_pos_x = clamp(0, text_pos_x, width)
-            text_pos_y = clamp(0, text_pos_y, height)
+            # text_pos_x = clamp(0, text_pos_x, width)
+            # text_pos_y = clamp(0, text_pos_y, height)
 
-            text_positions.append((text_pos_x, text_pos_y))
+            text_positions.append([text_pos_x, text_pos_y, tuple(img_array[pix_j, pix_i])])
 
     idraw = ImageDraw.Draw(result)
 
-    for pos in text_positions:
-        idraw.text(pos, "e", fill=0)
+    for x, y, color in text_positions:
+        # print(color, type(color))
+        idraw.text((x, y), "e", fill=color)
 
     # result.save(f'{sys.argv[1]} bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier}.png')
     return result
