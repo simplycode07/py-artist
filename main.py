6bc92265d1da64c4eb951a8b58b046e25c966e5a
simplycode07 <simplycode07@gmail.com>
2025-09-10 14:15:42 +0530

    performance improvement of 20% by using local variables and memoization

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ if len(sys.argv) <= 1:
     sys.exit()
 
 image_path = Path(sys.argv[1])
-block_size = int(sys.argv[2])
-effective_area = int(sys.argv[3])
-strength_multiplier = float(sys.argv[4])
-string = "e" if len(sys.argv) < 6 else sys.argv[5]
+block_size_glob = int(sys.argv[2])
+effective_area_glob = int(sys.argv[3])
+strength_multiplier_glob = float(sys.argv[4])
+string_glob = "e" if len(sys.argv) < 6 else sys.argv[5]
 
 
 def signof(i):
@@ -42,6 +42,8 @@ def clamp(start: int, value: int, end: int) -> int:
 
 def convert_image_to_art(original_image):
     # brightness = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    block_size = block_size_glob
+    effective_area = effective_area_glob
     brightness = original_image
     height, width, _ = brightness.shape
     brightness_arr = np.array(brightness)
@@ -59,12 +61,16 @@ def convert_image_to_art(original_image):
             avg = block.mean(axis=(0, 1)).astype(np.uint8)
 
             img_array[y//block_size - 1, x//block_size - 1] = avg[::-1]
-            img_array_bw[y//block_size - 1, x//block_size - 1] = int(np.mean(avg)) - 128
+            img_array_bw[y//block_size - 1, x //
+                         block_size - 1] = int(np.mean(avg)) - 128
 
     text_positions = []
 
     result = Image.new("RGB", (width, height), (255, 255, 255))
     pixels_done = 0
+
+    strength_multiplier = strength_multiplier_glob
+    string = string_glob
     for pix_j in range(0, len(img_array)):
         for pix_i in range(0, len(img_array[0])):
 
@@ -86,31 +92,21 @@ def convert_image_to_art(original_image):
                     if clamped_pix_i == pix_i and clamped_pix_j == pix_j:
                         continue
 
-                    brightness_pix = (
-                        int(np.mean(img_array[clamped_pix_j, clamped_pix_i])) - 128) / 128
-                    change_pos_x += brightness_pix * \
-                        dis(i, j) * strength_multiplier
-                    change_pos_y += brightness_pix * \
-                        dis(j, i) * strength_multiplier
-
-                    # change_pos_x += img_array_bw[clamped_pix_j, clamped_pix_i] * \
-                    #     dis(i, j) * strength_multiplier
-                    # change_pos_y += img_array_bw[clamped_pix_j, clamped_pix_i] * \
-                    #     dis(j, i) * strength_multiplier
+                    change_pos_x += img_array_bw[clamped_pix_j, clamped_pix_i] * \
+                        dis(i, j) * strength_multiplier / 128
+                    change_pos_y += img_array_bw[clamped_pix_j, clamped_pix_i] * \
+                        dis(j, i) * strength_multiplier / 128
 
             text_pos_x += round(change_pos_x)
             text_pos_y += round(change_pos_y)
 
-            # text_pos_x = clamp(0, text_pos_x, width)
-            # text_pos_y = clamp(0, text_pos_y, height)
-
-            text_positions.append([string[pixels_done % len(string)], text_pos_x, text_pos_y, tuple(img_array[pix_j, pix_i])])
+            text_positions.append([string[pixels_done % len(
+                string)], text_pos_x, text_pos_y, tuple(img_array[pix_j, pix_i])])
             pixels_done += 1
 
     idraw = ImageDraw.Draw(result)
 
     for chr, x, y, color in text_positions:
-        # print(color, type(color))
         idraw.text((x, y), chr, fill=color)
 
     # result.save(f'{sys.argv[1]} bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier}.png')
@@ -118,6 +114,9 @@ def convert_image_to_art(original_image):
 
 
 if __name__ == "__main__":
+    block_size = block_size_glob
+    effective_area = effective_area_glob
+
     if image_path.suffix in [".mp4", ".mov"]:
         original_video = cv2.VideoCapture(str(image_path))
         fps = int(original_video.get(cv2.CAP_PROP_FPS))
@@ -127,7 +126,7 @@ if __name__ == "__main__":
 
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # You can also try 'XVID'
         out = cv2.VideoWriter(
-            f'{sys.argv[1]} bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier}.mp4', fourcc, fps, (width, height))
+            f'{sys.argv[1]} bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier_glob}.mp4', fourcc, fps, (width, height))
 
         frame_count = 0
 
@@ -140,7 +139,8 @@ if __name__ == "__main__":
                 break
 
             converted_frame = np.array(convert_image_to_art(frame))
-            converted_frame_bgr = cv2.cvtColor(converted_frame, cv2.COLOR_RGB2BGR)
+            converted_frame_bgr = cv2.cvtColor(
+                converted_frame, cv2.COLOR_RGB2BGR)
             out.write(converted_frame_bgr)
             frame_count += 1
 
@@ -155,11 +155,11 @@ if __name__ == "__main__":
         out.release()
         cv2.destroyAllWindows()
         print(
-            f'bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier}')
+            f'bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier_glob}')
 
     else:
         converted_image = convert_image_to_art(cv2.imread(str(image_path)))
         print(
-            f'bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier}')
+            f'bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier_glob}')
         converted_image.save(
-            f'{sys.argv[1]} bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier}.png')
+            f'{sys.argv[1]} bl_size:{block_size} eff_area:{effective_area} stren_mul:{strength_multiplier_glob}.png')
